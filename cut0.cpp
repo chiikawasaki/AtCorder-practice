c2a3fab6c03f4aedc870aa7bba125ea1e429ac49
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-08-22 15:26:02 +0900

    AtCoder Beginner Contest 367 B問題　Cut.0

---
+++ cut0.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    string x;
+    cin >> x;
+    while(x[x.size()-1] == '0'){
+        x.pop_back();
+        }
+    if(x[x.size()-1] == '.'){
+        x.pop_back();
+    }
+    cout << x << endl;
+    return 0;
+}
